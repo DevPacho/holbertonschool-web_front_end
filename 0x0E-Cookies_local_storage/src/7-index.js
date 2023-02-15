3e6b053b12a91c10e6aecdb26b6d0295ac54ec0a
DevPacho <frankjose2004@gmail.com>
2023-02-14 21:18:20 -0500

    [FINALLY] Finished functions in advanced Web Storage management for shopping cart 😠

--- 0x0E-Cookies_local_storage/src/7-index.js
+++ 0x0E-Cookies_local_storage/src/7-index.js
@@ -78,7 +78,7 @@ const displayCart = () => {
 
 const updateCart = () => {
 
-  // Párchese que esta última task si estuvo muy tiesa rey jajaja 🤣😠
+  // Párchese que esta última task si estuvo muy tiesa rey jajajaja! 🤣😠
 
   const cartContent = getCartFromStorage();
   const divCart = document.getElementById("div-cart");
